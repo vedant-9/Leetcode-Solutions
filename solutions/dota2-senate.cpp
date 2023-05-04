27cc28c1ad3e3ae7fb506fd6a64b9fc0400d5bcd
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2023-05-04 11:15:27 +0530

    Create dota2-senate.cpp

---
+++ solutions/dota2-senate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> rq, dq;
+        for(int i=0; i<senate.size(); i++)
+            senate[i] == 'R' ? rq.push(i): dq.push(i);
+
+        while(!rq.empty() and !dq.empty()){
+            int a = rq.front(), b = dq.front();
+            rq.pop(), dq.pop();
+            if (a < b) 
+                rq.push(a+senate.size());
+            else
+                dq.push(b+senate.size());
+        }
+        
+		if (rq.size() > dq.size())
+			return "Radiant";
+		else
+			return "Dire";
+    }
+};
