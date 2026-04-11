96aed32e6aae6d0880a765aa49233d9fca8be481
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2026-04-11 18:37:22 +0530

    Implement h-index calculation using binary search

---
+++ solutions/h-index-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size(), lo = 0, hi = n - 1, ans = 0;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (citations[mid] >= n - mid) {
+                ans = n - mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
