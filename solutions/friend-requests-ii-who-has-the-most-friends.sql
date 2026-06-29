53182d0d043cf45981320b1cbf4d0d337c6c6822
Vedant Valsangkar <vedantvalsangkar@gmail.com>
2026-06-29 20:40:57 +0530

    Create SQL query for most friends
    Add SQL query to find the user with the most friends.

---
+++ solutions/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+WITH all_ids AS (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+)
+
+SELECT id, COUNT(id) AS num
+FROM all_ids
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
